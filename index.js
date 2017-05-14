fae2b4529cfc6eb6c833f4745c570aca32539743
Friðrik Laxdal Kárason <fridrik.laxdal@gmail.com>
2017-05-14 16:11:01 -0700

    working on Line Transform stream

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 'use strict';
 const fs = require('fs');
-const minimist = require('minimist')
+const stream = require('stream');
+const minimist = require('minimist');
 
 let argv = minimist(process.argv.slice(2));
 console.log(argv);
 
-//const source = fs.createReadStream('./logs/log_15.csv');
-const source = process.stdin;
+const source = fs.createReadStream('./logs/log_15.csv');
+//const source = process.stdin;
 
-source.pipe(process.stdout);
+//source.pipe(process.stdout);
+
+class LinerTransform extends stream.Transform{
+	constructor(){
+		super()
+	}
+	_transform(chunk, nec, next){
+		this.push(chunk);
+		next();
+	}
+	_flush(done){
+		done();
+	}
+}
+
+const liner = new LinerTransform();
+
+source.pipe(liner).pipe(process.stdout);
